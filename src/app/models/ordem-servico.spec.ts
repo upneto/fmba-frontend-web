41d75412a7affaaebd87e1cb83bdd489bb6e01bc
Ulisses Pereira da Silva Neto <upneto@gmail.com>
2023-01-07 18:52:58 -0300

    modelos

---
+++ src/app/models/ordem-servico.spec.ts
@@ -0,0 +1,7 @@
+import { OrdemServico } from './ordem-servico';
+
+describe('OrdemServico', () => {
+  it('should create an instance', () => {
+    expect(new OrdemServico()).toBeTruthy();
+  });
+});
---
+++ src/app/models/ordem-servico.ts
@@ -0,0 +1,9 @@
+export class OrdemServico {
+  id: number = 0;
+  dataCriacao: Date = new Date();
+  dataInicio: Date = new Date();
+  dataFinal: Date = new Date();
+  status: number = 0;
+  veiculo: number = 0;
+  cliente: number = 0;
+}
